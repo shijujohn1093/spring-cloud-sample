f85ab452fe249e11f8cc85a17ba26b913d238e52
“Shiju <shijujohn1093@gmail.com>
2024-05-20 22:48:51 +0530

    Code Changes

---
+++ src/test/java/com/shiju/springcloudsample/SpringCloudSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiju.springcloudsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
