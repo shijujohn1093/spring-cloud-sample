eb7da24a999dd019f254072624de42098bc7acd3
“Shiju <shijujohn1093@gmail.com>
2024-05-21 19:06:14 +0530

    code changes

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-mvn clean install
+#mvn clean install
+set -x #echo on
 cwd=$(pwd)
-nohup java -jar $cwd+"/eureka-service/target/*.jar" -Dserver.port=8761 &
-sleep 30s
-nohup java -jar $cwd+"/foo-service/target/*.jar" -Dserver.port=8081 &
-nohup java -jar $cwd+"/foo-service/target/*.jar" -Dserver.port=8082 &
-nohup java -jar $cwd+"/bar-service/target/*.jar" -Dserver.port=8083 &
-nohup java -jar $cwd+"/bar-service/target/*.jar" -Dserver.port=8084 &
+nohup java -jar $cwd/eureka-service/target/*.jar -Dserver.port=8761 &
+#sleep 30s
+nohup java -jar $cwd/foo-service/target/*.jar -Dserver.port=8081 &
+nohup java -jar $cwd/foo-service/target/*.jar -Dserver.port=8082 &
+nohup java -jar $cwd/bar-service/target/*.jar -Dserver.port=8083 &
+nohup java -jar $cwd/bar-service/target/*.jar -Dserver.port=8084 &
