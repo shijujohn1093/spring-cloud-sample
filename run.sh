1b43f42b32d450fef7aa317b2df45a377aac72c1
“Shiju <shijujohn1093@gmail.com>
2024-05-21 10:12:48 +0530

    Code Changes

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 mvn clean build
-(setsid "./config-service/run.sh" &);
-(setsid "./eureka-service/run.sh" &);
+(setsid "java -jar ./config-service/target/*.jar -Dserver.port=8888" &);
+(setsid "java -jar ./eureka-service/target/*.jar -Dserver.port=8761" &);
 sleep 30s
-(setsid "./foo-service/run.sh" &);
-(setsid "./bar-service/run.sh" &);
+(setsid "java -jar ./foo-service/target/*.jar -Dserver.port=8081" &);
+(setsid "java -jar ./foo-service/target/*.jar -Dserver.port=8082" &);
+(setsid "java -jar ./bar-service/target/*.jar -Dserver.port=8083" &);
+(setsid "java -jar ./bar-service/target/*.jar -Dserver.port=8084" &);
